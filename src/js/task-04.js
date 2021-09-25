6d23eda355d2dea58f39f688f4b877c1a0d8b5e2
yuriykolisnyk <ykolisnyk@gmail.com>
2021-09-25 12:03:02 +0300

    Update

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,3 +1,15 @@
+// Задание 4
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+// <div id="counter">
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </div>
+
 const counterValue = document.querySelector("#value");
 const incrementBtnEl = document.querySelector("#counter").lastElementChild;
 const decrementBtnEl = document.querySelector("#counter").firstElementChild;
--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,3 +1,10 @@
+// Задание 7
+// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+
+// <input id="font-size-control" type="range" />
+// <br />
+// <span id="text">Абракадабра!</span>
+
 const sizeInputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
